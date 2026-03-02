96a38d8ecd98e07089a994b19351d3ad5e4aec62
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2026-03-02 20:17:04 -0300

    Aplicando o styled-components
    Removendo todos os arquivos CSS e colocando tudo em JavaScript

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,19 +1,29 @@
-import "./estilo.css";
 import perfil from "../../imagens/perfil.svg";
 import sacola from "../../imagens/sacola.svg";
+import styled from "styled-components";
+
+const Icone = styled.li`
+  margin-right: 40px;
+  width: 25px;
+`;
+
+const Icones = styled.ul`
+  display: flex;
+  align-items: center;
+`;
 
 const icones = [perfil, sacola];
 
 function IconesHeader() {
-    return (
-        <ul className="icones">
-          {icones.map((icone) => (
-            <li className='icone'>
-              <img src={icone} alt="icone"/>
-            </li>
-          ))}
-        </ul>
-    )
+  return (
+    <Icones>
+      {icones.map((icone) => (
+        <Icone>
+          <img src={icone} alt="icone" />
+        </Icone>
+      ))}
+    </Icones>
+  );
 }
 
 export default IconesHeader;
